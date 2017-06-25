bcbf763ebfe8f3e8435e8c1bc27f240ac43afc95
Viktor Kotusenko <roxolanus@gmail.com>
2017-06-25 09:51:59 -0400

    giving docstring to events-overlap? function

--- src/dbooker/core.clj
+++ src/dbooker/core.clj
@@ -2,11 +2,13 @@
   (:require [clojure.pprint :as pp]
             [clojure.math.combinatorics :as combo]))
 
-(defn events-overlap? [pair]
+(defn events-overlap?
+  "Given the pair of events, checks if they overlap"
+  [pair]
   (let [[start1 end1] (first pair)
         [start2 end2] (second pair)]
     (assert (and (<= start1 end1) (<= start2 end2))
-            "Start of the event shoul be earlier than the end!")
+            "Start of the event should be earlier than the end!")
     (if (<= start1 start2)
       (> end1 start2)
       (> end2 start1))))
@@ -42,7 +44,7 @@
 ;; generating the calendar sequence (30 events, each sorted for start time < end time)
 (def calendar (map sort (partition 2 (take 30 (repeatedly #(rand-int 12)))))) 
 
-(pp/pprint (set (overlapping-1 calendar)))
-(pp/pprint (set (overlapping-2 calendar)))
-
+;; casting to sets because the sequence order in two cases is not guaranteed
 (= (set (overlapping-1 calendar)) (set (overlapping-2 calendar)))
+
+(pp/pprint (overlapping-1 calendar))
